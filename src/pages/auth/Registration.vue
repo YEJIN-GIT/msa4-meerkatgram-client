3a1f3e261ad81d085b9adcba6e04dce13f8ec257
YEJIN-GIT <yejinaissi@gmail.com>
2026-06-04 18:27:20 +0900

    260604 [develop] 파일업로드 처리. 회원가입 작업중.

--- src/pages/auth/Registration.vue
+++ src/pages/auth/Registration.vue
@@ -1,9 +1,69 @@
 <script setup>
+import { reactive, ref } from 'vue';
 import MyButton from '../../components/button/MyButton.vue';
 import MyInput from '../../components/input/MyInput.vue';
+import { useFileStore } from '../../store/file/useFileStore.js';
+import { useAuthStore } from '../../store/auth/useAuthStore.js';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const fileStore = useFileStore();
+const authStore = useAuthStore();
+
+const preview = ref(null);
+const selectedFile = ref(null);
+const registrationData = reactive({
+  email: '',
+  password: '',
+  passwordChk: '',
+  nick: '',
+  profile: '',
+});
 
 const handleSubmit = async () => {
+  try {
+    await authStore.registration(registrationData);
+    alert("회원가입에 성공했습니다.");
+    router.replace('/login');
+    
+  } catch (error) {
+    const data = error.response.data;
+    if(data.code === 'E11') {
+      alert(data.data);
+    } else if(data.code === 'E21') {
+      alert("잘못된 양식");
+    } else {
+      alert("오류가 발생했습니다.\n잠시후 다시 시도해 주십시오.");
+      router.replace('/');
+    }
+  }
+}
 
+const handleChangeProfile = async (e) => {
+  // target : 이벤트가 발생한 노드
+  // files : 파일정보가 배열형태로 들어가있음
+  const file = e.target.files[0];
+
+  if(file) {
+    if(preview.value) {
+      // 기존에 생성된 메모리 URL이 있다면 해제 (메모리 누수 방지)
+      // (브라우저가 임시경로에 저장해 둔 파일경로를 지움)
+      URL.revokeObjectURL(preview.value);
+    }
+
+    // API 서버에 파일 저장 요청
+    const fileUri = await fileStore.uploadProfile(file);
+
+    if(fileUri) {
+      registrationData.profile = fileUri;
+      
+      selectedFile.value = file;
+
+      // 파일 객체를 브라우저에서 접근 가능한 임시URL로 변환
+      preview.value = URL.createObjectURL(file);
+    }
+
+  }
 }
 </script>
 
@@ -13,28 +73,42 @@ const handleSubmit = async () => {
     :type="'email'"
     :placeholder="'Email'"
     :readonly="false"
-    :required="true"    
+    :required="true"
+    v-model="registrationData.email"
   ></MyInput>
   <MyInput
     :type="'password'"
     :placeholder="'Password'"
     :readonly="false"
-    :required="true"    
+    :required="true" 
+    v-model="registrationData.password"   
   ></MyInput>
   <MyInput
     :type="'password'"
     :placeholder="'Password Check'"
     :readonly="false"
-    :required="true"    
+    :required="true" 
+    v-model="registrationData.passwordChk"   
   ></MyInput>
   <MyInput
     :type="'text'"
     :placeholder="'Nick'"
     :readonly="false"
-    :required="true"    
+    :required="true"
+    v-model="registrationData.nick"
   ></MyInput>
   <!-- <label for="file">파일</label> -->
-  <input type="file" accept="image/*" id="file">
+  <!-- <input type="file" accept="image/*" id="file"> -->
+  <div 
+    class="preview"
+    v-if="preview"
+    :style="{backgroundImage: `url(${preview})`}"
+  ></div>
+  <input
+    type="file" 
+    accept="image/*"
+    @change="handleChangeProfile"
+  >
 
   <MyButton
   :btn-type="'submit'"
@@ -55,5 +129,14 @@ form {
   flex-direction: column;
   align-items: center;
   gap: 20px;
+}
+
+.preview {
+  width: 70px;
+  height: 70px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 50%;
 }
 </style>
--- src/store/auth/useAuthStore.js
+++ src/store/auth/useAuthStore.js
@@ -66,6 +66,18 @@ export const useAuthStore = defineStore('authStore', () => {
     }
   }
 
+  const registration = async (data) => {
+    try {
+      const url = '/api/registration';
+
+      await myAxios.post(url, data);
+      return;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
   return {
     // 1. State
     isLoggedIn,
@@ -79,6 +91,7 @@ export const useAuthStore = defineStore('authStore', () => {
     login,
     reissue,
     logout,
+    registration,
   }
 
 });
